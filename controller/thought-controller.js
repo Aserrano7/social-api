2ae063a0a61ff3906b9a700371d50d89c93bf318
Aserrano7 <serranoandres022@gmail.com>
2024-02-26 11:33:44 -0800

    added add and remove reaction

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -56,6 +56,38 @@ const ThoughtController = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  async deleteReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
 };
 
 module.exports = ThoughtController;
